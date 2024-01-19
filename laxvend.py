a88781372496491b7e79b879c0217f013618fcb6
xeniaMe <xeniamezencevadev@gmail.com>
2024-01-19 13:18:26 +0700

    поправила программу по методичке

--- laxvend.py
+++ laxvend.py
@@ -5,72 +5,114 @@ import matplotlib.pyplot as plt
 import warnings
 warnings.filterwarnings("ignore")
 
-N=40
-Nt = 10
-h = 2*np.pi/N #step size in X direction
-k = 1/Nt #step size in time (t) direction
-#r = k/(h*h)
+
+a = 0
+b = 1
+N = 40
+v = 1
+t_stop = 0.4
+t = 0 #текущее время
 c = 0.9 #Kurant number
-time_steps=10
-time=np.arange(0,(time_steps+.5)*k,k) #specified values for time (t) spacing in the grid
-x=np.arange(0,2*np.pi+h/2,h) #specified values for X spacing in the grid
-
-
-X, Y = np.meshgrid(x, time) #meshing
-
-#plot
-fig = plt.figure()
-plt.plot(X,Y,'ro');
-plt.plot(x,0*x,'bo',label='Initial Condition');
-plt.xlim((-h,2*np.pi+h))
-plt.ylim((-k,max(time)+k))
-plt.xlabel('x')
-plt.ylabel('time (ms)')
-plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
-plt.title(r'Discrete Grid ',fontsize=24,y=1.08)
-plt.show();
 
 #array for function values
-u=np.zeros((N+1,time_steps+1))
-#b=np.zeros(N-1)
-
-# Initial Condition for u
-for i in range (0,N+1):
-    u[i,0]=1-np.cos(x[i])
-    
-
-fig = plt.figure(figsize=(8,4))
-plt.plot(x,u[:,0],'o:',label='Initial Condition')
-plt.xlim([-0.1,max(x)+h])
-plt.title('Intitial Condition',fontsize=24)
-plt.xlabel('x')
-plt.ylabel('u')
-plt.legend(loc='best')
-plt.show()
-
-#border conditions
-ipos = np.zeros(N+1)
-ineg = np.zeros(N+1)
-
-for i in range(0,N+1):
-   ipos[i] = int(i+1)
-   ineg[i] = int(i-1)
-
-ipos[N] = 0
-ineg[0] = N
+u=np.linspace(0, N)
+u1 = np.linspace(0, N)
+xs = np.linspace(0,N)
 
 
-for j in range(0,time_steps):
-    for i in range (0,N+1):
-        u[i,j+1]=u[i,j]-c*(u[int(ipos[i]),j]-u[int(ineg[i]),j])+c*c/2*(u[int(ipos[i]),j]-2*u[i,j]+u[int(ineg[i]),j])
+#построение расчетной сетки 
+dx = (b-a)/ (N-1)
+dt = c * dx/v
+xs[0] = a
+
+for i in range (0, N):
+    xs[i] = xs[i-1] + dx
+
+
+#определение начальных и граничных условий
+def U0(x):
+    if x <= 0.2:
+        return 0.5
+    elif x <= 0.4:
+        return 1.0
+    else:
+        return 0.5
+
+
+def SetIC():
+    #global t, u, xs
+    t = 0.0
+    for i in range(N):
+        u[i] = U0(xs[i])
+
+def SetBC():
+    u1[0] = 5 #u[0] - c*(u[0] - u[N-2])
+    u1[N-1] = 10 #u[N-1] - c*(u[N-1] - u[N-2])
+
+
+# расчет
+def Step():
+    for i in range ( 0, N-1 ):
+        u1[i] = u[i] - c*(u[i]-u[i]) + c*c/2*(u[i] - 2*u[i]+u[i])
+    #print(u1)
         
-fig = plt.figure(figsize=(12,6))
 
-plt.subplot(121)
-for j in range (1,time_steps+1):
-    plt.plot(x,u[:,j],'o:')
-plt.xlabel('x')
-plt.ylabel('u')
+#обновление НУ
+def UpdateIC():
+    for i in range ( 0, N):
+        u[i] = u1[i]
 
 
-plt.show()
+
+while t <= t_stop:
+    SetBC()  
+    Step()   
+    UpdateIC()  
+    t += dt
+
+def SaveData():
+    try:
+        with open("C:/Рабочий стол/work1/data.txt", "w") as f:
+            f.write("#x u\n")
+            for i in range(len(xs)):
+                f.write(f"{xs[i]} {u1[i]}\n")
+    except IOError:
+        print("unable to open file for writing")
+    #f.close()
+
+SaveData()
+
+
+
+
+
+
+
+
+
+#plot
+# fig = plt.figure()
+# plt.plot(X,Y,'ro');
+# plt.plot(x,0*x,'bo',label='Initial Condition');
+# plt.xlim((-h,2*np.pi+h))
+# plt.ylim((-k,max(time)+k))
+# plt.xlabel('x')
+# plt.ylabel('time (ms)')
+# plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+# plt.title(r'Discrete Grid ',fontsize=24,y=1.08)
+# plt.show();
+# fig = plt.figure(figsize=(8,4))
+# plt.plot(x,u[:,0],'o:',label='Initial Condition')
+# plt.xlim([-0.1,max(x)+h])
+# plt.title('Intitial Condition',fontsize=24)
+# plt.xlabel('x')
+# plt.ylabel('u')
+# plt.legend(loc='best')
+# plt.show()       
+# fig = plt.figure(figsize=(12,6))
+# plt.subplot(121)
+# for j in range (1,time_steps+1):
+#     plt.plot(x,u[:,j],'o:')
+# plt.xlabel('x')
+# plt.ylabel('u')
+# plt.show()
